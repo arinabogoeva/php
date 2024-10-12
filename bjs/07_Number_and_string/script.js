bde4f5953e6210e9d554537d82002673bcc4fad2
Arina Bogoeva <bogoevamd@gmail.com>
2024-10-12 22:21:45 +0300

    first commit

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -10,3 +10,112 @@ document.getElementById('btn_clr').addEventListener('click', function () {
     inputWindow.value = '';
 })
 
+document.getElementById('btn_1').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.getElementById('btn_2').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+document.getElementById('btn_3').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+
+document.getElementById('btn_sum').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sum';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_calc').addEventListener('click', function () {
+    if (operation === 'sum') {
+        const result = lastOperand + parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'dif') {
+        const result = lastOperand - parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'prod') {
+        const result = lastOperand * parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'quot') {
+        const result = lastOperand / parseInt(inputWindow.value);
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+    if (operation === 'sq') {
+        const result = parseInt(Math.sqrt(lastOperand));
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
+document.getElementById('btn_4').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+document.getElementById('btn_5').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+document.getElementById('btn_6').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+document.getElementById('btn_dif').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'dif';
+    inputWindow.value = '';
+})
+
+document.getElementById('btn_7').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+document.getElementById('btn_8').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+document.getElementById('btn_9').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+
+document.getElementById('btn_prod').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'prod';
+    inputWindow.value = '';
+})
+
+
+document.getElementById('btn_0').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
+
+document.getElementById('btn_quot').addEventListener('click', function () {
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'quot';
+    inputWindow.value = '';
+})
+document.getElementById('btn_sq').addEventListener('click', function () {
+    
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'sq';
+    inputWindow.value = '';
+    const result = parseInt(Math.sqrt(lastOperand));
+    operation = null;
+    lastOperand = 0;
+    inputWindow.value = result;
+    
+})
+
+
+
